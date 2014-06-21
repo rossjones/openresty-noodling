e41b8e389794e94f1a6a6123545076ed1dc8466b
Ross Jones <ross@servercode.co.uk>
2014-06-21 15:01:54 +0100

    Semi-useful comments

--- src/webapp.lua
+++ src/webapp.lua
@@ -11,7 +11,19 @@ local before_fns = {};
 local after_fns = {};
 
 ---
+--- Add a before and after function (either optional) that will
+--- be called before and after the request is complete.  The
+--- before function is always added at the end if the queue of
+--- functions to be run, and the after always at the start.  This
+--- means they'll be called as follows:
 ---
+---    webapp.middleware(a, b)
+---    webapp.middleware(x, z)
+---
+---    a()
+---    x()
+---    z()
+---    b()
 ---
 function middleware(before, after)
     if before ~= nil then
@@ -23,7 +35,7 @@ function middleware(before, after)
 end
 
 ---
----
+--- Render the context in the provided template name.
 ---
 function render(template_name, context)
     --ngx.header.content_type = 'text/html';
@@ -34,21 +46,28 @@ function render(template_name, context)
 end
 
 
+---
+--- Let the user provide urls and the functions they should be
+--- routed to.  The urls should be regular expressions, not contain the
+--- root / and look something like:
+---
+--- webapp.routes({
+---    ['$']      = index,
+---    ['about$'] = about,
+---    ['hello/(?<first>.*)/(?<second>.*)$'] = hello,
+--- })
+---
+--- The named parameters in the 'hello' url will be passed in that order
+--- to the function.  They don't need to be named parameters, this is
+--- just in case I can work out how to pass them as named parameters ;)
+---
 function routes(user_routes)
     for pattern, view in pairs(user_routes) do
         local uri = '^/'.. pattern
-        local match = ngx.re.match(ngx.var.uri, uri, "") -- regex mather in compile mode
+        local match = ngx.re.match(ngx.var.uri, uri, "")
         if match then
             for i, fn in ipairs(before_fns) do
                 fn()
-            end
-
-            local args={}
-
-            for k,v in pairs(match) do
-                if match[k] then
-                    args[k]=k
-                end
             end
 
             -- We should unpack match so that we can pass the arguments that match in order
